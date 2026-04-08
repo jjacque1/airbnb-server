f73a0ca37bbc032e2e69262313670353e7319bdd
jjacque1 <jacksonjacque.jj@gmail.com>
2026-04-08 16:15:37 -0400

    server starts successfully after dependencies reinstalled

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ async function startServer() {
 
     const PORT = process.env.PORT || 5001;
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (err) {
     console.error("Server failed to start:", err.message);
