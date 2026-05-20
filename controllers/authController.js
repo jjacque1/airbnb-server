ba882a7b6479047b5eb530909a0ac2d474caff9f
jjacque1 <jacksonjacque.jj@gmail.com>
2026-05-20 09:31:34 -0400

    changed cookies token setting, samesite from lax to none

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,7 +55,7 @@ export async function registerUser(req, res) {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -100,8 +100,8 @@ export async function loginUser(req, res) {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
       secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
